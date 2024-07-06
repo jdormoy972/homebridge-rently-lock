eb20a0c94dfeb805d011a6b78d27ffcc939bc93d
jdormoy972 <jeromedormoy2@gmail.com>
2024-07-05 20:32:24 -0400

    Updated gitignore

---
+++ dist/platform.ts
@@ -0,0 +1,123 @@
+import {
+  API,
+  Characteristic,
+  DynamicPlatformPlugin,
+  Logging,
+  PlatformAccessory,
+  PlatformConfig,
+  Service,
+} from "homebridge";
+
+import { RentlyLockAccessory } from "./platformAccessory.js";
+import { RentlyAPI } from "./rently-api/RentlyAPI.js";
+import { PLATFORM_NAME, PLUGIN_NAME } from "./settings.js";
+import { RentlyLock } from "./types.js";
+
+/**
+ * HomebridgePlatform
+ * This class is the main constructor for your plugin, this is where you should
+ * parse the user config and discover/register accessories with Homebridge.
+ */
+export class HomebridgeRentlyLock implements DynamicPlatformPlugin {
+  public readonly Service: typeof Service;
+  public readonly Characteristic: typeof Characteristic;
+  public readonly RentlyAPI: RentlyAPI;
+
+  // this is used to track restored cached accessories
+  public readonly accessories: PlatformAccessory<RentlyLock>[] = [];
+
+  constructor(
+    public readonly log: Logging,
+    public readonly config: PlatformConfig,
+    public readonly api: API
+  ) {
+    this.Service = api.hap.Service;
+    this.Characteristic = api.hap.Characteristic;
+    this.RentlyAPI = new RentlyAPI(this.config.email, this.config.password);
+
+    this.log.debug("Finished initializing platform:", this.config.name);
+
+    // Homebridge 1.8.0 introduced a `log.success` method that can be used to log success messages
+    // For users that are on a version prior to 1.8.0, we need a 'polyfill' for this method
+    if (!log.success) {
+      log.success = log.info;
+    }
+
+    // When this event is fired it means Homebridge has restored all cached accessories from disk.
+    // Dynamic Platform plugins should only register new accessories after this event was fired,
+    // in order to ensure they weren't added to homebridge already. This event can also be used
+    // to start discovery of new accessories.
+    this.api.on("didFinishLaunching", () => {
+      log.debug("Executed didFinishLaunching callback");
+      // run the method to discover / register your devices as accessories
+      this.registerLocks();
+    });
+  }
+
+  /**
+   * This function is invoked when homebridge restores cached accessories from disk at startup.
+   * It should be used to set up event handlers for characteristics and update respective values.
+   */
+  configureAccessory(accessory: PlatformAccessory<RentlyLock>) {
+    this.log.info("Loading accessory from cache:", accessory.displayName);
+
+    // add the restored accessory to the accessories cache, so we can track if it has already been registered
+    this.accessories.push(accessory);
+  }
+
+  async registerLocks() {
+    this.log.info("Registering locks");
+
+    await this.RentlyAPI.login();
+
+    const rentlyLocks = await this.RentlyAPI.getAllPropertyDevices();
+
+    for (const rentlyLock of rentlyLocks) {
+      const uuid = this.api.hap.uuid.generate(rentlyLock.id);
+      const existingAccessory = this.accessories.find(
+        (accessory) => accessory.UUID === uuid
+      );
+
+      if (existingAccessory) {
+        // the accessory already exists
+        this.log.info(
+          "Restoring existing accessory from cache:",
+          existingAccessory.displayName
+        );
+
+        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. e.g.:
+        // existingAccessory.context.device = device;
+        // this.api.updatePlatformAccessories([existingAccessory]);
+
+        // create the accessory handler for the restored accessory
+        // this is imported from `platformAccessory.ts`
+        new RentlyLockAccessory(this, existingAccessory, this.RentlyAPI);
+
+        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, e.g.:
+        // remove platform accessories when no longer present
+        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
+        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
+      } else {
+        // the accessory does not yet exist, so we need to create it
+        this.log.info("Adding new accessory:", rentlyLock.device_name);
+
+        // create a new accessory
+        const accessory: PlatformAccessory<RentlyLock> =
+          new this.api.platformAccessory(rentlyLock.device_name, uuid);
+
+        // store a copy of the device object in the `accessory.context`
+        // the `context` property can be used to store any data about the accessory you may need
+        accessory.context = rentlyLock;
+
+        // create the accessory handler for the newly create accessory
+        // this is imported from `platformAccessory.ts`
+        new RentlyLockAccessory(this, accessory, this.RentlyAPI);
+
+        // link the accessory to your platform
+        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [
+          accessory,
+        ]);
+      }
+    }
+  }
+}
---
+++ dist/platformAccessory.ts
@@ -0,0 +1,99 @@
+import { CharacteristicValue, PlatformAccessory, Service } from "homebridge";
+
+import { HomebridgeRentlyLock } from "./platform.js";
+import { RentlyAPI } from "./rently-api/RentlyAPI.js";
+import { RentlyLock } from "./types.js";
+
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
+export class RentlyLockAccessory {
+  private service: Service;
+
+  constructor(
+    private readonly platform: HomebridgeRentlyLock,
+    private readonly accessory: PlatformAccessory<RentlyLock>,
+    private RentlyApi: RentlyAPI
+  ) {
+    // set accessory information
+    this.accessory
+      .getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, "Rently")
+      .setCharacteristic(
+        this.platform.Characteristic.Model,
+        this.accessory.context.device_type
+      )
+      .setCharacteristic(
+        this.platform.Characteristic.SerialNumber,
+        this.accessory.context.id
+      );
+
+    // get the LockMechanism service if it exists, otherwise create a new LockMechanism service
+    this.service =
+      this.accessory.getService(this.platform.Service.LockMechanism) ||
+      this.accessory.addService(this.platform.Service.LockMechanism);
+
+    // set the service name, this is what is displayed as the default name on the Home app
+    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+    this.service.setCharacteristic(
+      this.platform.Characteristic.Name,
+      accessory.context.device_name
+    );
+
+    // each service must implement at-minimum the "required characteristics" for the given service type
+    // see https://developers.homebridge.io/#/service/LockMechanism
+
+    this.service
+      .getCharacteristic(this.platform.Characteristic.LockCurrentState)
+      .onGet(this.handleLockCurrentStateGet.bind(this));
+
+    this.service
+      .getCharacteristic(this.platform.Characteristic.LockTargetState)
+      .onGet(this.handleLockCurrentStateGet.bind(this)) // idk if there's a different endpoint for this
+      .onSet(this.handleLockTargetStateSet.bind(this));
+  }
+
+  /**
+   * Handle "SET" requests from HomeKit
+   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
+   */
+  async handleLockTargetStateSet(value: CharacteristicValue) {
+    this.RentlyApi.setLockedState(this.accessory.context.id, value as boolean);
+    this.platform.log.debug("Set Characteristic Locked ->", value);
+  }
+
+  /**
+   * Handle the "GET" requests from HomeKit
+   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
+   *
+   * GET requests should return as fast as possible. A long delay here will result in
+   * HomeKit being unresponsive and a bad user experience in general.
+   *
+   * If your device takes time to respond you should update the status of your device
+   * asynchronously instead using the `updateCharacteristic` method instead.
+
+   * @example
+   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
+   */
+  async handleLockCurrentStateGet(): Promise<CharacteristicValue> {
+    const isLocked = await this.RentlyApi.getLockedState(
+      this.accessory.context.id
+    );
+    const characteristicValue = isLocked
+      ? this.platform.Characteristic.LockCurrentState.SECURED
+      : this.platform.Characteristic.LockCurrentState.UNSECURED;
+
+    this.platform.log.debug("Get Characteristic IsLocked ->", isLocked);
+    this.platform.log.debug(
+      "Get Characteristic CharacteristicValue ->",
+      characteristicValue
+    );
+
+    // if you need to return an error to show the device as "Not Responding" in the Home app:
+    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+
+    return isLocked;
+  }
+}
